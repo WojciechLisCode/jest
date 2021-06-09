1c2a6504a438e292319a9ae12a510cef258c55fa
WojciechLisCode <wlis58372+code@gmail.com>
2021-06-09 17:27:44 +0200

    testing with mock

--- src/store/spaces/tests/actions.test.js
+++ src/store/spaces/tests/actions.test.js
@@ -1,4 +1,11 @@
-import { FETCH_SPACES_SUCCESS, fetchSpacesSuccess } from "../actions";
+import {
+  FETCH_SPACES_SUCCESS,
+  fetchSpacesSuccess,
+  fetchSpaces,
+} from "../actions";
+
+import axios from "axios";
+jest.mock("axios");
 
 describe("#fetchSpacesSuccess", () => {
   describe("if given an array of spaces", () => {
@@ -15,6 +22,23 @@ describe("#fetchSpacesSuccess", () => {
   describe("if given a null argument", () => {
     test("action.payload should be null", () => {
       expect(fetchSpacesSuccess(null).payload).toBeNull();
+    });
+  });
+});
+
+jest.mock("axios");
+
+describe("#fetchSpaces", () => {
+  describe("when called", () => {
+    test("should dispatch an action FETCH_SPACES_SUCCESS", async () => {
+      const fakeSpaces = [{}, {}];
+      const response = { data: { spaces: { rows: fakeSpaces } } };
+      axios.get.mockImplementationOnce(() => Promise.resolve(response));
+      const dispatch = jest.fn();
+      const getState = jest.fn().mockReturnValueOnce({ spaces: [] });
+      await fetchSpaces()(dispatch, getState);
+      expect(dispatch).toHaveBeenCalledWith(fetchSpacesSuccess(fakeSpaces));
+      expect(getState).toHaveBeenCalledTimes(1);
     });
   });
 });
